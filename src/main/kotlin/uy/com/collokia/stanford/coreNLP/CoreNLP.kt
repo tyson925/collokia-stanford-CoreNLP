2a3dbb65183446a309bfa3166b6cfb1ee50db117
Istvan Nagy <tyson925@gmail.com>
2016-08-16 17:40:09 +0200

    change to dataset -much faster

--- src/main/kotlin/uy/com/collokia/stanford/coreNLP/CoreNLP.kt
+++ src/main/kotlin/uy/com/collokia/stanford/coreNLP/CoreNLP.kt
@@ -6,6 +6,7 @@ import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations
 import org.apache.spark.ml.Transformer
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.DataTypes
@@ -13,7 +14,7 @@ import org.apache.spark.sql.types.StructType
 import java.io.Serializable
 import java.util.*
 
-public data class ParsedSentenceBean(val category : String, val categoryIndex : Double, val content : String,var tokens: List<String>, var poses: List<String>, var lemmas: List<String>,
+public data class ParsedSentenceBean(var category: String, var categoryIndex: Double, var content: String, var tokens: List<String>, var poses: List<String>, var lemmas: List<String>,
                                      var parses: List<String>, var ners: List<String>) : Serializable
 
 
@@ -64,7 +65,17 @@ public class CoreNLP : Transformer {
     override fun transform(dataset: Dataset<*>?): Dataset<Row>? {
 
 
-        val paesedContent = dataset?.select(dataset.col("category"),dataset.col("categoryIndex"),dataset.col(inputColName))?.javaRDD()?.map { text ->
+        val isIndex = dataset?.columns()?.toList()?.contains("categoryIndex") ?: false
+
+        val selectContent = if (isIndex) {
+            dataset?.select(dataset.col("category"), dataset.col("categoryIndex"), dataset.col(inputColName))
+        } else {
+            dataset?.select(dataset.col("category"), dataset.col(inputColName))
+        }
+        val beanEncoder = Encoders.bean(ParsedSentenceBean::class.java)
+        val parsedContent = selectContent?.map({ text ->
+
+        //val parsedContent = selectContent?.javaRDD()?.map { text ->
             val content = text.getString(text.fieldIndex(inputColName))
             val doc = Annotation(content)
             wrapper.get()?.annotate(doc)
@@ -122,23 +133,45 @@ public class CoreNLP : Transformer {
 
             val res = if (!annotations.contains("parse") && !annotations.contains("ner")) {
                 outputCol = "lemmas"
-                ParsedSentenceBean(text.getString(0),text.getDouble(1),content,tokens, poses, lemmas, ArrayList<String>(), ArrayList<String>())
+                val index = if (isIndex) {
+                    text.getDouble(1)
+                } else {
+                    0.0
+                }
+                ParsedSentenceBean(text.getString(0), index, content, tokens, poses, lemmas, ArrayList<String>(), ArrayList<String>())
 //                res = RowFactory.create(text.getLong(0), tokens, poses, lemmas)
             } else if (annotations.contains("parse") && !annotations.contains("ner")) {
                 outputCol = "parses"
                 //               res = RowFactory.create(text.getLong(0), tokens, poses, lemmas, parses)
-                ParsedSentenceBean(text.getString(0),text.getDouble(1),content,tokens, poses, lemmas, parses, ArrayList<String>())
+                val index = if (isIndex) {
+                    text.getDouble(1)
+                } else {
+                    0.0
+                }
+                ParsedSentenceBean(text.getString(0), index, content, tokens, poses, lemmas, parses, ArrayList<String>())
             } else if (annotations.contains("parse") && annotations.contains("ner")) {
                 outputCol = "ners"
 //                res = RowFactory.create(text.getLong(0), tokens, poses, lemmas, parses, ners)
-                ParsedSentenceBean(text.getString(0),text.getDouble(1),content,tokens, poses, lemmas, parses, ners)
+                val index = if (isIndex) {
+                    text.getDouble(1)
+                } else {
+                    0.0
+                }
+                ParsedSentenceBean(text.getString(0), index, content, tokens, poses, lemmas, parses, ners)
             } else {
-                ParsedSentenceBean(text.getString(0),text.getDouble(1),content,tokens, ArrayList<String>(), ArrayList<String>(), ArrayList<String>(), ArrayList<String>())
+                val index = if (isIndex) {
+                    text.getDouble(1)
+                } else {
+                    0.0
+                }
+                ParsedSentenceBean(text.getString(0), index, content, tokens, ArrayList<String>(), ArrayList<String>(), ArrayList<String>(), ArrayList<String>())
             }
             res
-        }
+        }, beanEncoder)
         //val encoder = Encoders.bean(ParsedSentenceBean::class.java)
-        return sparkSession.createDataFrame(paesedContent, ParsedSentenceBean::class.java)
+        //return sparkSession.createDataFrame(parsedContent, ParsedSentenceBean::class.java)
+        parsedContent?.show(3)
+        return parsedContent?.toDF()
     }
 
 
---
+++ src/main/kotlin/uy/com/collokia/stanford/coreNLP/DocumentCoreNLP.kt
@@ -0,0 +1,4 @@
+package uy.com.collokia.stanford.coreNLP
+
+
+
