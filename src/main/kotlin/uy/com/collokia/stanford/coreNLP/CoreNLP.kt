89938e448e69de5f211c0e8cc6df269b5788ad37
Istvan Nagy <tyson925@gmail.com>
2016-08-22 19:04:26 +0200

    store so into es

--- src/main/kotlin/uy/com/collokia/stanford/coreNLP/CoreNLP.kt
+++ src/main/kotlin/uy/com/collokia/stanford/coreNLP/CoreNLP.kt
@@ -11,12 +11,31 @@ import org.apache.spark.sql.types.StructType
 import java.io.Serializable
 import java.util.*
 
-public data class ParsedSentenceBean(var category: String, var categoryIndex: Double, var content: String, var title : String,
-                                     var labels : List<String>, var tokens: String, var poses: String, var lemmas: String,
+public data class ParsedSentenceBean(var category: String, var categoryIndex: Double, var content: String, var title: String,
+                                     var labels: List<String>, var tokens: String, var poses: String, var lemmas: String,
                                      var parses: String, var ners: String) : Serializable
 
 
 public class CoreNLP : Transformer {
+
+    var wrapper: StanfordCoreNLPWrapper
+    var sparkSession: SparkSession
+    var annotations: String
+    //var inputColName: String by Delegates.notNull<String>()
+    var inputColName: String? = null
+    var outputCol: String? = "lemmas"
+
+    constructor(sparkSession: SparkSession, annotations: String) {
+        this.sparkSession = sparkSession
+        this.annotations = annotations
+
+        //CoreNLP("corenlp_" + UUID.randomUUID().toString().drop(12))
+        val props = Properties()
+        //tokenize,
+        props.setProperty("annotators", "tokenize, ssplit, ${annotations}")
+        wrapper = StanfordCoreNLPWrapper(props)
+    }
+
     override fun copy(p0: ParamMap): Transformer {
         return CoreNLP(sparkSession, annotations)
     }
@@ -42,24 +61,6 @@ public class CoreNLP : Transformer {
         return res
     }
 
-    var wrapper: StanfordCoreNLPWrapper
-    var sparkSession: SparkSession
-    var annotations: String
-    //var inputColName: String by Delegates.notNull<String>()
-    var inputColName: String? = null
-    var outputCol: String? = "lemmas"
-
-    constructor(sparkSession: SparkSession, annotations: String) {
-        this.sparkSession = sparkSession
-        this.annotations = annotations
-
-        //CoreNLP("corenlp_" + UUID.randomUUID().toString().drop(12))
-        val props = Properties()
-        //tokenize,
-        props.setProperty("annotators", "tokenize, ssplit, ${annotations}")
-        wrapper = StanfordCoreNLPWrapper(props)
-    }
-
 
     override fun transform(dataset: Dataset<*>?): Dataset<Row>? {
 
@@ -67,119 +68,119 @@ public class CoreNLP : Transformer {
         val isTitle = dataset?.columns()?.toList()?.contains("title") ?: false
         val isLabels = dataset?.columns()?.toList()?.contains("labels") ?: false
         val colums = LinkedList<Column>()
-        dataset?.let {
+        return dataset?.let {
             colums.add(dataset.col("category"))
-            if (isIndex){
+            if (isIndex) {
                 colums.add(dataset.col("categoryIndex"))
             }
             colums.add(dataset.col("content"))
-            if (isTitle){
+            if (isTitle) {
                 colums.add(dataset.col("title"))
             }
-            if (isLabels){
+            if (isLabels) {
                 colums.add(dataset.col("labels"))
             }
             dataset.col(inputColName)
-        }
 
-        val columsSeq = scala.collection.JavaConversions.asScalaBuffer(colums)
+            val columsSeq = scala.collection.JavaConversions.asScalaBuffer(colums)
 
-        val selectContent = dataset?.select(columsSeq)
+            val selectContent = dataset.select(columsSeq)
 
-        val beanEncoder = Encoders.bean(ParsedSentenceBean::class.java)
-        val parsedContent = selectContent?.map({ text ->
+            val beanEncoder = Encoders.bean(ParsedSentenceBean::class.java)
+            val parsedContent = selectContent.map({ text ->
 
-            //val parsedContent = selectContent?.javaRDD()?.map { text ->
-            val content = text.getString(text.fieldIndex(inputColName))
-            val doc = Annotation(content)
-            wrapper.get()?.annotate(doc)
-            val sentences = doc.get(CoreAnnotations.SentencesAnnotation::class.java)
+                //val parsedContent = selectContent?.javaRDD()?.map { text ->
+                val content = text.getString(text.fieldIndex(inputColName))
+                val doc = Annotation(content)
+                wrapper.get()?.annotate(doc)
+                val sentences = doc.get(CoreAnnotations.SentencesAnnotation::class.java)
 //            val numberOfSentences = sentences.size
-            //val tokens = ArrayList<List<String>>(numberOfSentences)
-            val tokens = LinkedList<String>()
-            val poses = LinkedList<String>()
-            val lemmas = LinkedList<String>()
-            val parses = LinkedList<String>()
-            val ners = LinkedList<String>()
+                //val tokens = ArrayList<List<String>>(numberOfSentences)
+                val tokens = LinkedList<String>()
+                val poses = LinkedList<String>()
+                val lemmas = LinkedList<String>()
+                val parses = LinkedList<String>()
+                val ners = LinkedList<String>()
 
-            for ((sentenceIndex, sentence) in sentences.withIndex()) {
-                val documentTokensList = sentence.get(CoreAnnotations.TokensAnnotation::class.java)
-                //println(sentence)
+                for ((sentenceIndex, sentence) in sentences.withIndex()) {
+                    val documentTokensList = sentence.get(CoreAnnotations.TokensAnnotation::class.java)
+                    //println(sentence)
 
 
-                val tokenArray = documentTokensList.map { token ->
-                    token.get(CoreAnnotations.TextAnnotation::class.java)
-                }
-                //tokens.add(sentenceIndex,tokenArray)
-                tokens.addAll(tokenArray)
-
-                val posArray = documentTokensList.map { token ->
-                    token.get(CoreAnnotations.PartOfSpeechAnnotation::class.java)
-                }
-
-                poses.addAll(posArray)
-
-                val lemmaArray = documentTokensList.map { token ->
-                    token.get(CoreAnnotations.LemmaAnnotation::class.java)
-                }
-                lemmas.addAll(lemmaArray)
-
-                if (annotations.contains("parse")) {
-
-                    val dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation::class.java)
-                    //SemanticGraph dependencies = sentence.get(CoreAnnotations.CoNLLDepAnnotation.class);
-                    val edgeList = dependencies.edgeListSorted()
-
-                    val parseArray = edgeList.map { edge ->
-                        edge.toString()
+                    val tokenArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.TextAnnotation::class.java)
                     }
-                    parses.addAll(parseArray)
-                }
+                    //tokens.add(sentenceIndex,tokenArray)
+                    tokens.addAll(tokenArray)
 
-                if (annotations.contains("ner")) {
-                    val nerArray = documentTokensList.map { token ->
-                        token.get(CoreAnnotations.NamedEntityTagAnnotation::class.java)
+                    val posArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.PartOfSpeechAnnotation::class.java)
                     }
-                    ners.addAll(nerArray)
+
+                    poses.addAll(posArray)
+
+                    val lemmaArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.LemmaAnnotation::class.java)
+                    }
+                    lemmas.addAll(lemmaArray)
+
+                    if (annotations.contains("parse")) {
+
+                        val dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation::class.java)
+                        //SemanticGraph dependencies = sentence.get(CoreAnnotations.CoNLLDepAnnotation.class);
+                        val edgeList = dependencies.edgeListSorted()
+
+                        val parseArray = edgeList.map { edge ->
+                            edge.toString()
+                        }
+                        parses.addAll(parseArray)
+                    }
+
+                    if (annotations.contains("ner")) {
+                        val nerArray = documentTokensList.map { token ->
+                            token.get(CoreAnnotations.NamedEntityTagAnnotation::class.java)
+                        }
+                        ners.addAll(nerArray)
+                    }
                 }
-            }
 
-            val index = if (isIndex) {
-                text.getDouble(1)
-            } else {
-                0.0
-            }
-            val title = if (isTitle) {
-                text.getString(text.fieldIndex("title"))
-            } else {
-                ""
-            }
+                val index = if (isIndex) {
+                    text.getDouble(1)
+                } else {
+                    0.0
+                }
+                val title = if (isTitle) {
+                    text.getString(text.fieldIndex("title"))
+                } else {
+                    ""
+                }
 
-            val labels = if (isLabels){
-                text.getList<String>(text.fieldIndex("labels"))
-            } else {
-                listOf()
-            }
+                val labels = if (isLabels) {
+                    text.getList<String>(text.fieldIndex("labels"))
+                } else {
+                    listOf()
+                }
 
-            val res = if (!annotations.contains("parse") && !annotations.contains("ner")) {
-                outputCol = "lemmas"
-                ParsedSentenceBean(text.getString(0), index, content, title,labels,tokens.joinToString(" "), poses.joinToString(" "),
-                        lemmas.joinToString(" "), "", "")
-            } else if (annotations.contains("parse") && !annotations.contains("ner")) {
-                outputCol = "parses"
-                ParsedSentenceBean(text.getString(0), index, content, title, labels,tokens.joinToString(" "), poses.joinToString(" "),
-                        lemmas.joinToString(" "), parses.joinToString(" "), "")
-            } else if (annotations.contains("parse") && annotations.contains("ner")) {
-                outputCol = "ners"
-                ParsedSentenceBean(text.getString(0), index, content, title,labels,tokens.joinToString(" "), poses.joinToString(" "),
-                        lemmas.joinToString(" "), parses.joinToString(" "), ners.joinToString(" "))
-            } else {
-                ParsedSentenceBean(text.getString(0), index, content, title,labels,tokens.joinToString(" "), "", "", "", "")
-            }
-            res
-        }, beanEncoder)
+                val res = if (!annotations.contains("parse") && !annotations.contains("ner")) {
+                    outputCol = "lemmas"
+                    ParsedSentenceBean(text.getString(0), index, content, title, labels, tokens.joinToString(" "), poses.joinToString(" "),
+                            lemmas.joinToString(" "), "", "")
+                } else if (annotations.contains("parse") && !annotations.contains("ner")) {
+                    outputCol = "parses"
+                    ParsedSentenceBean(text.getString(0), index, content, title, labels, tokens.joinToString(" "), poses.joinToString(" "),
+                            lemmas.joinToString(" "), parses.joinToString(" "), "")
+                } else if (annotations.contains("parse") && annotations.contains("ner")) {
+                    outputCol = "ners"
+                    ParsedSentenceBean(text.getString(0), index, content, title, labels, tokens.joinToString(" "), poses.joinToString(" "),
+                            lemmas.joinToString(" "), parses.joinToString(" "), ners.joinToString(" "))
+                } else {
+                    ParsedSentenceBean(text.getString(0), index, content, title, labels, tokens.joinToString(" "), "", "", "", "")
+                }
+                res
+            }, beanEncoder)
+            parsedContent.toDF()
+        } ?: dataset?.toDF()
 
-        return parsedContent?.toDF()
     }
 
 
---
+++ src/main/kotlin/uy/com/collokia/stanford/coreNLP/CoreNLPSO.kt
@@ -0,0 +1,125 @@
+package uy.com.collokia.stanford.coreNLP
+
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.pipeline.Annotation
+import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.types.StructType
+import java.io.Serializable
+import java.util.*
+
+
+public data class ParsedSoPage(var parsedContent: String, var title: String, var codes: List<String>,
+                               var imports: List<String>, var tags : List<String>) : Serializable
+
+
+public class CoreNLPSO : Transformer {
+
+    var wrapper: StanfordCoreNLPWrapper
+    var sparkSession: SparkSession
+    var annotations: String
+    //var inputColName: String by Delegates.notNull<String>()
+    var inputColName: String? = null
+    var outputCol: String? = "lemmas"
+
+    constructor(sparkSession: SparkSession, annotations: String) {
+        this.sparkSession = sparkSession
+        this.annotations = annotations
+
+        //CoreNLP("corenlp_" + UUID.randomUUID().toString().drop(12))
+        val props = Properties()
+        //tokenize,
+        props.setProperty("annotators", "tokenize, ssplit, ${annotations}")
+        wrapper = StanfordCoreNLPWrapper(props)
+    }
+
+
+    override fun copy(p0: ParamMap): Transformer {
+        return CoreNLPSO(sparkSession, annotations)
+    }
+
+    override fun transformSchema(p0: StructType?): StructType? {
+        //throw UnsupportedOperationException()
+        var res = p0?.add(DataTypes.createStructField("parsedContent", DataTypes.StringType, false))
+
+        res = res?.add(DataTypes.createStructField("title", DataTypes.StringType, false))
+
+        res = res?.add(DataTypes.createStructField("imports", DataTypes.createArrayType(DataTypes.StringType), false))
+        res = res?.add(DataTypes.createStructField("codes", DataTypes.createArrayType(DataTypes.StringType), false))
+        res = res?.add(DataTypes.createStructField("tags", DataTypes.createArrayType(DataTypes.StringType), false))
+
+        return res
+    }
+
+    override fun transform(dataset: Dataset<*>?): Dataset<Row>? {
+
+        return dataset?.let {
+            val colums = listOf(dataset.col("title"), dataset.col("codes"), dataset.col("imports"), dataset.col("tags"), dataset.col(inputColName))
+            val columsSeq = scala.collection.JavaConversions.asScalaBuffer(colums)
+            val selectContent = dataset.select(columsSeq)
+            val beanEncoder = Encoders.bean(ParsedSoPage::class.java)
+
+            val parsedContent = selectContent.map({ text ->
+
+                //val parsedContent = selectContent?.javaRDD()?.map { text ->
+                val content = text.getString(text.fieldIndex(inputColName))
+                val doc = Annotation(content)
+                wrapper.get()?.annotate(doc)
+                val sentences = doc.get(CoreAnnotations.SentencesAnnotation::class.java)
+                val numberOfSentences = sentences.size
+                //val tokens = ArrayList<List<String>>(numberOfSentences)
+                val tokens = LinkedList<String>()
+                val poses = LinkedList<String>()
+                val lemmas = LinkedList<String>()
+
+                for ((sentenceIndex, sentence) in sentences.withIndex()) {
+                    val documentTokensList = sentence.get(CoreAnnotations.TokensAnnotation::class.java)
+                    //println(sentence)
+
+
+                    val tokenArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.TextAnnotation::class.java)
+                    }
+                    //tokens.add(sentenceIndex,tokenArray)
+                    tokens.addAll(tokenArray)
+
+                    val posArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.PartOfSpeechAnnotation::class.java)
+                    }
+
+                    poses.addAll(posArray)
+
+                    val lemmaArray = documentTokensList.map { token ->
+                        token.get(CoreAnnotations.LemmaAnnotation::class.java)
+                    }
+                    lemmas.addAll(lemmaArray)
+
+                }
+
+                val title = text.getString(text.fieldIndex("title"))
+                val codes = text.getList<String>(text.fieldIndex("codes"))
+                val imports = text.getList<String>(text.fieldIndex("imports"))
+                val tags = text.getList<String>(text.fieldIndex("tags"))
+                ParsedSoPage(lemmas.joinToString(" "), title, codes, imports,tags)
+
+            }, beanEncoder)
+
+            parsedContent?.toDF()
+        } ?: dataset?.toDF()
+
+    }
+
+    override fun uid(): String? {
+        return UUID.randomUUID().toString()
+    }
+
+    public fun setInputCol(inputCol: String): CoreNLPSO {
+        inputColName = inputCol
+        return this
+    }
+}
