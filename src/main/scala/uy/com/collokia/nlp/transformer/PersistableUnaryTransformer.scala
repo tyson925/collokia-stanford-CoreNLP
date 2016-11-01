fb6edc81bd00c8ff6b85d500a5573962bd1e198e
Gyorgy Mora <gymora@gmail.com>
2016-11-01 02:51:37 -0700

    add identifiable interface

--- src/main/scala/uy/com/collokia/nlp/transformer/PersistableUnaryTransformer.scala
+++ src/main/scala/uy/com/collokia/nlp/transformer/PersistableUnaryTransformer.scala
@@ -2,11 +2,11 @@
 package uy.com.collokia.nlp.transformer
 
 import org.apache.spark.ml.UnaryTransformer
-import org.apache.spark.ml.util.{DefaultParamsReadable, DefaultParamsWritable}
+import org.apache.spark.ml.util.{DefaultParamsReadable, DefaultParamsWritable, Identifiable}
 
 
 
-abstract class PersistableUnaryTransformer[IN, OUT, T<:UnaryTransformer[IN, OUT, T]] extends UnaryTransformer[IN, OUT, T] with DefaultParamsReadable[T] with DefaultParamsWritable
+abstract class PersistableUnaryTransformer[IN, OUT, T<:UnaryTransformer[IN, OUT, T]] extends UnaryTransformer[IN, OUT, T] with DefaultParamsReadable[T] with DefaultParamsWritable with Identifiable
 
 
 
