2e3dfd016cf59b321ff2b582af4a176511bebdda
Gyorgy Mora <gymora@gmail.com>
2016-11-01 02:16:41 -0700

    trick scala

--- src/main/kotlin/uy/com/collokia/nlp/transformer/OwnNGram.kt
+++ src/main/kotlin/uy/com/collokia/nlp/transformer/OwnNGram.kt
@@ -1,39 +1,24 @@
 package uy.com.collokia.nlp.transformer
 
-import org.apache.spark.ml.UnaryTransformer
-import org.apache.spark.ml.util.DefaultParamsWritable
-import org.apache.spark.ml.util.MLWritable
-import org.apache.spark.ml.util.MLWriter
 import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.types.DataTypes
 import scala.Function1
 import scala.collection.mutable.WrappedArray
-import kotlin.reflect.jvm.reflect
-
-class OwnNGram : UnaryTransformer<WrappedArray<String>, Array<String>, OwnNGram>(), DefaultParamsWritable {
-
-    override fun write(): MLWriter {
-
-        return MLWritable::write.call("OwnNGram")
-    }
-
-    override fun save(path: String) {
-        MLWritable::save.call(path)
-    }
 
 
-    override fun createTransformFunc(): Function1<WrappedArray<String>, Array<String>> {
+class OwnNGram : PersistableUnaryTransformer<WrappedArray<String>, Array<String>, OwnNGram>() {
+    override fun createTransformFunc(): Function1<WrappedArray<String>,Array<String>>? {
         return ConvertFunction()
     }
 
-
-    override fun outputDataType(): DataType {
+    override fun outputDataType(): DataType? {
         return DataTypes.createArrayType(DataTypes.StringType)
     }
 
-    override fun uid(): String {
-        //return UUID.randomUUID().toString()
-        return "uid1111111"
+    override fun uid(): String? {
+        throw UnsupportedOperationException()
     }
+
+
 }
 
