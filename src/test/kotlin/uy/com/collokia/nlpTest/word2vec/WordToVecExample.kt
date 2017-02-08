7a7e24ad000a5304caa23325958ee4c5efb1fb45
Istvan Nagy <tyson925@gmail.com>
2017-02-08 11:48:26 +0100

    gany of netty

--- src/test/kotlin/uy/com/collokia/nlpTest/word2vec/WordToVecExample.kt
+++ src/test/kotlin/uy/com/collokia/nlpTest/word2vec/WordToVecExample.kt
@@ -1,19 +1,29 @@
 package uy.com.collokia.nlpTest.word2vec
 
+import uy.com.collokia.common.utils.measureTimeInMillis
+import uy.com.collokia.common.utils.rdd.closeSpark
 import uy.com.collokia.common.utils.rdd.getLocalSparkContext
 
 class WordToVecExample() {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            val jsc = getLocalSparkContext("spark")
+            val time = measureTimeInMillis {
+                val jsc = getLocalSparkContext("spark")
 
-            val corpus = jsc.textFile("./../collokia-data-es-indexer/data/word2vec/spanish/text.txt").map { line ->
-                line.split(" ") as Iterable<String>
+                val corpus = jsc.textFile("./../collokia-data-es-indexer/data/word2vec/spanish/text.txt").map { line ->
+                    line.split(" ") as Iterable<String>
+                }
+
+                val model = org.apache.spark.mllib.feature.Word2Vec().fit(corpus)
+
+
+                model.save(jsc.sc(), "./data/word2vec/spanish")
+                model.transform("deporte")
+                println(model.findSynonyms("deporte", 4).joinToString("\n"))
+
+                closeSpark(jsc)
             }
-
-            val model = org.apache.spark.mllib.feature.Word2Vec().fit(corpus)
-            model.transform("London")
-            println(model.findSynonyms("London", 4))
+            println("Excecution time was ${time.second}")
 
         }
 
